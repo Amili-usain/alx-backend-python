5d68a6b08da70aeb6dddd7f3919c9d7dbe64b1b5
Amili-usain <amilihussein073@gmail.com>
2023-04-27 11:44:28 +0000

    advanced task 3

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python3
-
-"""
-This module provides a function to zoom in a given tuple by repeating each
-element a certain number of times.
-"""
-
-from typing import Tuple, List, Any
+"""This module provides a function to zoom in a given tuple by repeating
+each element a certain number of times."""
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple[Any], factor: int = 2) -> Tuple:
-    """
-    Zooms in the given tuple by repeating each element a certain number o
-times.
-
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Zooms in the given tuple by repeating each element a certain number
+of times.
     Args:
-        lst: The input tuple to zoom in.
-        factor: The factor by which to zoom in. Defaults to 2.
-
+        lst: The input tuple to zoom in
+        factor: An integer  to zoom in. Defaults to 2
     Returns:
-        The zoomed in tuple.
-    """
-    zoomed_in: List[Any] = [
+           The zoomed in tuple.
+     """
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
-if __name__ == "__main__":
-    array = (12, 72, 91)
+array: Tuple = (12, 72, 91)
 
-    zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-    zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3)
