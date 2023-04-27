c15e1bc647bbc00536fc597182d71e0c2f05e131
Amili-usain <amilihussein073@gmail.com>
2023-04-27 05:23:27 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the input float number.
+
+    Args:
+        n (float): The float number to convert to string.
+
+    Returns:
+        str: The string representation of the input number.
+    """
+    return str(n)
