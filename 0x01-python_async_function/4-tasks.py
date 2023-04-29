3a5e1ae820889b93aeffc24b1733b4fe5b31d49c
Amili-usain <amilihussein073@gmail.com>
2023-04-29 10:25:45 +0000

    Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,19 +7,9 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Asynchronous coroutine that spawns task_wait_random n times with the
-specified max_delay.
-
-    Args:
-        n (int): Number of times to spawn the coroutine.
-        max_delay (int): Maximum delay in seconds for each coroutine.
-
-    Returns:
-        List[float]: List of all the delays (float values) in ascending order.
-    """
+    """Asynchronous coroutine that spawns task_wait_random n times with the
+specified max_delay."""
     tasks = []
     delays = []
 
